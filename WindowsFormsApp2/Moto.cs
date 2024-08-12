21a5282a6528a51260cf58ed4dfd040af29fd4bf
VirStockli <virstockli@gmail.com>
2024-08-11 21:38:45 -0300

    Guia 1 - Ejercicio 1 complete

---
+++ WindowsFormsApp2/Moto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Moto
+    {
+        public string Marca { get; private set; }
+        public int Modelo { get; private set; }
+        public double ValorFabricacion { get; private set; }
+
+        public Moto(string marca, int modelo, double valorFabricacion)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            ValorFabricacion = valorFabricacion;
+        }
+        public double CalcularDepreciacionLineal(int añoaCalcular, int vidaUtil)
+        {
+            double añosdeuso = añoaCalcular - Modelo;
+            double ValorActualizado = ValorFabricacion - (ValorFabricacion * (añosdeuso / vidaUtil));
+
+            return ValorActualizado;
+        }
+        public double CalcularDepreciacionAnual(int añoaCalcular, double tasaDepreciacion)
+        {
+            double añosdeuso = añoaCalcular - Modelo;
+            double ValorActualizado = ValorFabricacion * Math.Pow((1 - tasaDepreciacion), añosdeuso);
+            return ValorActualizado;
+        }
+
+        public string VerDescripcion()
+        {
+            string descripcion = $"Marca: {Marca} - Modelo: {Modelo} - Valor Fabricación U$S{ValorFabricacion}";
+            return descripcion;
+        }
+
+        //HELP HELP HELP SOS
+    }
+}
---
+++ WindowsFormsApp2/Tas.cs
@@ -0,0 +1,53 @@
+﻿using Ejercicio1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Tas : Form
+    {
+       
+        public Tas()
+        {
+            InitializeComponent();
+        }
+
+        public void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Ver resultados = new Ver();
+
+            //resultados.lbVer.Items.Clear();
+
+            string marca = tbMarca.Text;
+            int modelo = Convert.ToInt32(nudModelo.Value);
+            double valor = Convert.ToDouble(tbValor.Text);
+            //int año = Convert.ToInt32(nudAño.Text);
+            //int vida = Convert.ToInt32(tbVida.Text);
+            //double tasa = Convert.ToDouble(tbTasa.Text);
+
+            Moto motito;
+
+            motito = new Moto(marca, modelo, valor);
+                          
+           double lineal = motito.CalcularDepreciacionLineal(Convert.ToInt32(nudAño.Text), Convert.ToInt32(tbVida.Text));
+                    
+           double anual = motito.CalcularDepreciacionAnual(Convert.ToInt32(nudAño.Text), Convert.ToDouble(tbTasa.Text));
+                
+           string datos = motito.VerDescripcion();             
+
+                resultados.lbVer.Items.Add($"{datos}");
+                resultados.lbVer.Items.Add($"Depreciación lineal: {lineal,10:f2}");
+                resultados.lbVer.Items.Add($"Depreciación anual: {anual,10:f2}");
+                resultados.ShowDialog();
+            
+
+        }
+    }
+}
